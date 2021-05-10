c71a0a6c8def61cbebdb248515f3eedb070850a4
Haifeng Zhao <hf_zhao@yeah.net>
2021-05-10 09:55:56 +0800

    [+] 加密存入数据库

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const sha1 = require('js-sha1');
 const sql = require('../src/dao/sql');
 
 // 用户登录
 router.post('/login', (req, res) => {
   let formData = req.body;
+  formData['pwd'] = sha1(formData['pwd']).slice(0, 18);
+  console.log(formData);
   sql.login(formData, res);
 });
 
 // 用户注册
 router.post('/register', (req, res) => {
   let formData = req.body;
+  formData['pwd'] = sha1(formData['pwd']).slice(0, 18);
   sql.register(formData, res);
 });
 
