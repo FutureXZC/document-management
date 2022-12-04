ffd489d67221212e76e8a991b500aef8daff02bd
FutureXZC <1073739700@qq.com>
2022-12-04 17:46:39 +0800

    [!] 注册页面添加备案号，修复logo显示异常

--- frontend/src/views/register/index.vue
+++ frontend/src/views/register/index.vue
@@ -1,60 +1,63 @@
 <template>
-  <div class="login-wrap">
-    <div>
-      <img src="static/images/logo.png" alt="曦开实验室" class="logo" />
-      <img src="static/images/title.png" alt="文件管理系统" class="title" />
+  <div>
+    <div class="login-wrap">
+      <div>
+        <img src="static/images/logo.png" alt="曦开实验室" class="logo" />
+        <img src="static/images/title.png" alt="文件管理系统" class="title" />
+      </div>
+      <el-form
+        ref="registerForm"
+        :model="form"
+        :rules="rules"
+        label-width="80px"
+        class="login-box"
+      >
+        <h3 class="login-title">用户注册</h3>
+        <el-form-item label="账号" prop="id" size="large">
+          <div class="login-form-wrap">
+            <el-input
+              type="text"
+              placeholder="账号建议使用您的工号"
+              v-model="form.id"
+            />
+          </div>
+        </el-form-item>
+        <el-form-item label="用户名" prop="name" size="large">
+          <div class="login-form-wrap">
+            <el-input
+              type="text"
+              placeholder="用户名应为您的姓名"
+              v-model="form.name"
+            />
+          </div>
+        </el-form-item>
+        <el-form-item label="密码" prop="pwd">
+          <div class="login-form-wrap">
+            <el-input
+              placeholder="可输入6-18位字母、数字和下划线"
+              v-model="form.pwd"
+              show-password
+            />
+          </div>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="pwd2">
+          <div class="login-form-wrap">
+            <el-input
+              placeholder="请再次输入密码"
+              v-model="form.pwd2"
+              show-password
+            />
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <div class="login-btn">
+            <el-button v-on:click="register">注册</el-button>
+            <el-button type="primary" v-on:click="toLogin">前往登录</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
     </div>
-    <el-form
-      ref="registerForm"
-      :model="form"
-      :rules="rules"
-      label-width="80px"
-      class="login-box"
-    >
-      <h3 class="login-title">用户注册</h3>
-      <el-form-item label="账号" prop="id" size="large">
-        <div class="login-form-wrap">
-          <el-input
-            type="text"
-            placeholder="账号建议使用您的工号"
-            v-model="form.id"
-          />
-        </div>
-      </el-form-item>
-      <el-form-item label="用户名" prop="name" size="large">
-        <div class="login-form-wrap">
-          <el-input
-            type="text"
-            placeholder="用户名应为您的姓名"
-            v-model="form.name"
-          />
-        </div>
-      </el-form-item>
-      <el-form-item label="密码" prop="pwd">
-        <div class="login-form-wrap">
-          <el-input
-            placeholder="可输入6-18位字母、数字和下划线"
-            v-model="form.pwd"
-            show-password
-          />
-        </div>
-      </el-form-item>
-      <el-form-item label="确认密码" prop="pwd2">
-        <div class="login-form-wrap">
-          <el-input
-            placeholder="请再次输入密码"
-            v-model="form.pwd2"
-            show-password
-          />
-        </div>
-      </el-form-item>
-      <el-form-item>
-        <div class="login-btn">
-          <el-button v-on:click="register">注册</el-button>
-          <el-button type="primary" v-on:click="toLogin">前往登录</el-button>
-        </div>
-      </el-form-item>
-    </el-form>
+    <div class="icp-record-blue">苏ICP备19034356号-1</div>
   </div>
 </template>
 
@@ -174,7 +177,8 @@ export default {
 }
 
 .title {
-  margin: 35px 90px;
+  width: 640px;
+  margin: 64px 90px;
 }
 
 .login-box {
@@ -204,5 +208,20 @@ export default {
 
 .login-btn {
   text-align: center;
+}
+
+.icp-record-blue {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  color: white;
+  font-size: 16px;
+  line-height: 40px;
+  text-align: center;
+  background-color: #68a3be;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
